84116417bd42aa2eccec381fb232bd7557533bf8
Michael Chesser <michaelchesser@outlook.com>
2016-04-20 17:15:55 +0930

    Host docs

---
+++ doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hackrf_sys'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='hackrf_sys/enum.hackrf_error.html' title='hackrf_sys::hackrf_error'>hackrf_error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='hackrf_sys/enum.hackrf_board_id.html' title='hackrf_sys::hackrf_board_id'>hackrf_board_id</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='hackrf_sys/enum.hackrf_usb_board_id.html' title='hackrf_sys::hackrf_usb_board_id'>hackrf_usb_board_id</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='hackrf_sys/enum.rf_path_filter.html' title='hackrf_sys::rf_path_filter'>rf_path_filter</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='hackrf_sys/enum.transceiver_mode_t.html' title='hackrf_sys::transceiver_mode_t'>transceiver_mode_t</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
